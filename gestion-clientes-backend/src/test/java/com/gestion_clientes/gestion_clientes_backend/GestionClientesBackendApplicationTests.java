470d86dbb7f2791f3d8007977d297354f11c8dfb
Juankos0714 <juankos0714@gmail.com>
2025-07-16 00:36:48 -0500

    First Commit

---
+++ gestion-clientes-backend/src/test/java/com/gestion_clientes/gestion_clientes_backend/GestionClientesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion_clientes.gestion_clientes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionClientesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
