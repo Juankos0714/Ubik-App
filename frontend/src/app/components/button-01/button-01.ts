bd4764435cf15aa5f668d3a8e4a41e74b1e63e09
Nicolasld222 <nicolaslondia@gmail.com>
2025-12-01 20:45:31 -0500

    Agrego la vista home y algunos components

--- frontend/src/app/components/button-01/button-01.ts
+++ frontend/src/app/components/button-01/button-01.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { text } from 'node:stream/consumers';
 
 @Component({
   selector: 'app-button-01',
@@ -8,5 +9,5 @@ import { RouterLink } from "@angular/router";
   styleUrl: './button-01.css',
 })
 export class Button01 {
-
+  @Input() text! : string;
 }
