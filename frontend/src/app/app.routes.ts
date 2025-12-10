190068b9bbcca1716cc17eb830ab933a1ff664c7
Miguel-A-VN <mvargasnavarro19@gmail.com>
2025-12-10 09:17:01 -0500

    checkponit Antes de hacer merge con frontend-components Home - Login - Frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import path from 'path';
 import { Home } from './views/home/home';
+import { Explore } from './views/explore/explore';
 
 export const routes: Routes = [
     {path: "", component: Home},
+    {path: "explore", component: Explore},
+    
         
 ];
--- frontend/src/app/views/explore/explore.ts
+++ frontend/src/app/views/explore/explore.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+export interface Motel {
+  id: number;
+  name: string;
+  category: string;
+  location: string;
+  price: number;
+  features: string[]; // jacuzzi, sauna, etc.
+}
+
 @Component({
   selector: 'app-explore',
   imports: [],
@@ -8,7 +17,53 @@ import { Component } from '@angular/core';
 })
 export class Explore {
 
-  datosEstaticos = [
-    {id: 1, nameService: "Servicio"},
-  ]
+  motels: Motel[] = [
+    {
+      id: 1,
+      name: 'Oasis',
+      category: 'Premium',
+      location: 'Bogotá',
+      price: 150000,
+      features: ['Jacuzzi', 'Parqueadero', 'TV']
+    },
+    {
+      id: 2,
+      name: 'Luna Azul',
+      category: 'Económico',
+      location: 'Medellín',
+      price: 80000,
+      features: ['Parqueadero']
+    }
+  ];
+
+  filters = {
+    category: '',
+    location: '',
+    priceMin: 0,
+    priceMax: 999999,
+    features: [] as string[] // checkboxes
+  };
+
+  get filteredMotels(): Motel[] {
+  return this.motels.filter(m =>
+    (this.filters.category ? m.category === this.filters.category : true) &&
+    (this.filters.location ? m.location === this.filters.location : true) &&
+    m.price >= this.filters.priceMin &&
+    m.price <= this.filters.priceMax &&
+    this.filters.features.every(f => m.features.includes(f))
+  );
+  
+  }
+
+  onFeatureChange(event: Event) {
+  const input = event.target as HTMLInputElement;
+  const value = input.value;
+
+    if (input.checked) {
+      this.filters.features.push(value);
+    } else {
+      this.filters.features = this.filters.features.filter(f => f !== value);
+    }
+  }
+
 }
