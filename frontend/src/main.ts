9b2f7908ae4266a94bdcbf67078ccb91c8b11e8b
Nicolasld222 <nicolaslondia@gmail.com>
2026-01-28 22:29:10 -0500

    Toca importar zone.js para traer las habitaciones

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,5 @@
+import 'zone.js';
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
