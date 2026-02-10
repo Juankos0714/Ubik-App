cf21d157ad0511142dc4290bd758a6c66ec065dd
Miguel-A-VN <mvargasnavarro19@gmail.com>
2026-02-10 18:22:34 -0500

    Se agregaron los modelos - de forma global

---
+++ frontend/src/app/core/models/room.model.ts
@@ -0,0 +1,13 @@
+import { RoomService } from './room_services.model';
+import { RoomImage } from './room_images.model';
+
+export interface Room {
+  id: number;
+  motel_id: number;
+  num_or_name: string;
+  room_type: string;
+  price: number;
+  description: string;
+  services?: RoomService[];
+  photos?: RoomImage[];
+}
