3f46e0a675eb193d9705f78412d00d37bafdc69d
Nicolasld222 <nicolaslondia@gmail.com>
2026-02-09 20:41:05 -0500

    Modifique el header para los roles, tambien agregue para terminar la view de propiedes

--- frontend/src/app/layout/header/header.ts
+++ frontend/src/app/layout/header/header.ts
@@ -1,14 +1,57 @@
-import { Component } from '@angular/core';
-import { Logo01 } from "../../components/logo-01/logo-01";
-import { RouterLink } from "@angular/router";
-import { Button01 } from "../../components/button-01/button-01";
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+import { Logo01 } from '../../components/logo-01/logo-01';
+import { Button01 } from '../../components/button-01/button-01';
+
+import { AuthService } from '../../core/auth.service';
+import { routes } from '../../app.routes';  
+import { RoomsOfferts } from '../../views/rooms-offerts/rooms-offerts';
+
+const ROUTES = {
+  HOME: '/',
+  EXPLORE: '/explore',
+  LOGIN: '/login',
+  REGISTER: '/select-register',
+  PROFILE: '/userProfile', 
+  OWNER: '/', 
+  ADMIN: '/' ,
+  OFFERTS: '/'
+};
 
 @Component({
   selector: 'app-header',
-  imports: [Logo01, RouterLink, Button01],
+  standalone: true,
+  imports: [Logo01, Button01],
   templateUrl: './header.html',
 })
-
 export class Header {
- logo : string = 'https://res.cloudinary.com/du4tcug9q/image/upload/v1763473322/Logo_Ubik_jxgzqi.png';
+
+  // ===== Inject =====
+  private router = inject(Router);
+  private auth = inject(AuthService);
+
+  ROUTES = ROUTES;
+
+  // ===== Logo =====
+  logo: string =
+    'https://res.cloudinary.com/du4tcug9q/image/upload/v1763473322/Logo_Ubik_jxgzqi.png';
+
+  // ===== Signals del Auth =====
+  isLogged = this.auth.isLogged;
+  role = this.auth.role;
+
+  // ===== Navegación =====
+  AppRoutes = routes;
+
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
+
+  // ===== Logout =====
+  logout() {
+    this.auth.logout();
+    this.router.navigate([ROUTES.HOME]);
+  }
 }
+
