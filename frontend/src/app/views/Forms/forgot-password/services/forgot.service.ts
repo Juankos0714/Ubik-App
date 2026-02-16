74cf80240bed7182aad5c26683ff03000d87ffbd
Nicolasld222 <nicolaslondia@gmail.com>
2026-02-16 08:36:02 -0500

    organice recuperacion de contraseñas

--- frontend/src/app/views/Forms/forgot-password/services/forgot.service.ts
+++ frontend/src/app/views/Forms/forgot-password/services/forgot.service.ts
@@ -6,16 +6,21 @@ import { Observable } from "rxjs";
 @Injectable({ providedIn: 'root' })
 export class ForgotService {
 
-  private baseUrl = `${environment.apiUrl}/auth/reset-password`;
+  private baseUrl = `${environment.apiUrl}/auth`;
 
   constructor(private http: HttpClient) {}
 
+  // STEP 1 → Enviar email
   requestReset(email: string): Observable<any> {
-    return this.http.post(this.baseUrl, { email });
+    return this.http.post(
+      `${this.baseUrl}/reset-password-request?email=${email}`,
+      {}
+    );
   }
 
+  // STEP 2 → Cambiar contraseña
   resetPassword(token: string, newPassword: string): Observable<any> {
-    return this.http.post(this.baseUrl, {
+    return this.http.post(`${this.baseUrl}/reset-password`, {
       token,
       newPassword
     });
