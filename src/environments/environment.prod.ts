4b94873b650c0298a33ee7b5ff919a18e2bd04d9
Juankos0714 <juankos0714@gmail.com>
2026-02-26 19:37:24 -0500

    fix prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://ubik-back.duckdns.org/api',
   cloudinary: {
-    cloudName: 'du4tcuq9q',
+    cloudName: 'du4tcug9q',
     uploadPreset: 'angular_images', 
     folder: 'Ubik',
   }
