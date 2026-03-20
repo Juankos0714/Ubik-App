7caed053ae0160741ba1af229e8efacc8891c54f
Juankos0714 <juankos0714@gmail.com>
2026-03-20 08:34:39 -0500

    chore: update API URL to ubik.eastus2.cloudapp.azure.com

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://ubik-back.duckdns.org/api',
-  authUrl: 'https://ubik-back.duckdns.org/auth',
+  apiUrl: 'https://ubik.eastus2.cloudapp.azure.com/api',
+  authUrl: 'https://ubik.eastus2.cloudapp.azure.com/auth',
   googleClientId: '672241408590-k1d1i93sddk9lieh6ova9adq2f42kimf.apps.googleusercontent.com',
   cloudinary: {
     cloudName: 'du4tcug9q',
